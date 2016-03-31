092ec7081002b3e0c86670f8b211dc7ed870e5b3
Krystan Honour <krystan@gmail.com>
2016-03-31 08:41:08 +0100

    moved manifest to json file

--- index.js
+++ index.js
@@ -1,59 +1,7 @@
 'use strict';
 const Glue = require('glue');
 const Pack = require('./package');
-
- const manifest = {
-     connections: [{
-         port: 3000,
-         labels: ["api"],
-         routes: {cors: true}}],
-     registrations: [
-         {
-             plugin: {
-                 register: 'good',
-                 options: {
-                     reporters: [{
-                         reporter: require('good-console'),
-                         events: {
-                             response: '*',
-                             log: '*'
-                         }
-                     }]
-                 }
-             }
-         },
-         {
-             plugin: {
-                 register: 'inert',
-                 options: {select: 'api', options: {}}
-             }
-         },
-         {
-             plugin: {
-                 register: 'vision',
-                 options: {}
-             }
-         },
-         {
-             plugin: {
-                 register: 'hapi-swagger',
-                 options: {info: {title: "My Api", version: Pack.version}, tags: [{name: 'api'}]}
-             }
-         },
-         {
-             plugin: {
-                 register: 'chairo',
-                 options: {}
-             }
-         },
-         {
-             plugin: {
-                 register: './names',
-                 options: {}
-             }
-         }
-     ]
- };
+const manifest = require('./config/manifest.json')
 
 const options = {
     relativeTo: __dirname + '/modules',
